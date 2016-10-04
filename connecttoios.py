cfb61c1f53b0f7fd8c21a5b357c42e6c9a3afc34
Joe Liberi <m4punk@gmail.com>
2016-10-04 16:32:21 -0400

    Created ConnectAndSend class

--- connecttoios.py
+++ connecttoios.py
@@ -1,6 +1,44 @@
 #!/usr/local/bin/python3
 
-import paramiko, time, os, sys
+import paramiko, time, os
+
+class ConnectToIOS():
+
+	def __init__(self, ip, username, password, cmd):
+		self.ip = ip
+		self.username = username
+		self.password = password
+		self.cmd = cmd
+
+	def ConnectAndSend(self):
+
+		ssh=paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+		print("Opening connection please wait.....\n")
+		ssh.connect(self.ip, username=self.username, password=self.password,look_for_keys=False, allow_agent=False)
+
+		remote_conn = ssh.invoke_shell()
+		# Turn off paging
+		disable_paging(remote_conn)
+
+		send_enable(remote_conn, password)
+
+		output = send_command(remote_conn, self.cmd)
+		# # output = remote_conn.recv(5000)
+
+		# print(output.decode('ascii'))
+		# # timeout = 3.0
+
+		# # endtime = time.time() + timeout
+
+		# # while not stdout.channel.eof_received:
+		# # 	time.sleep(1)
+		# # 	if time.time() > endtime:
+		# # 		stdout.channel.close()
+		# # 		break
+
+		# # print(stdout.read())
 
 def send_command(shell, cmd):
 
@@ -17,47 +55,3 @@ def send_enable(shell, password):
 	shell.send('enable\n')
 	shell.send(password + '\n')
 	time.sleep(1)
-
-def disable_paging(remote_conn):
-    '''Disable paging on a Cisco router'''
-
-    remote_conn.send("terminal length 0\n")
-    time.sleep(1)
-
-    # Clear the buffer on the screen
-    output = remote_conn.recv(1000)
-
-    return output
-
-if __name__ == '__main__':
-
-	ip = '192.168.123.7'
-	username = 'admin'
-	password = '*******'
-	ssh=paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-	print("Opening connection please wait.....\n")
-	ssh.connect(ip, username=username, password=password,look_for_keys=False, allow_agent=False)
-
-	remote_conn = ssh.invoke_shell()
-	# Turn off paging
-	disable_paging(remote_conn)
-
-	send_enable(remote_conn, password)
-
-	output = send_command(remote_conn, sys.argv[1])
-	# output = remote_conn.recv(5000)
-
-	print(output.decode('ascii'))
-	# timeout = 3.0
-
-	# endtime = time.time() + timeout
-
-	# while not stdout.channel.eof_received:
-	# 	time.sleep(1)
-	# 	if time.time() > endtime:
-	# 		stdout.channel.close()
-	# 		break
-
-	# print(stdout.read())
